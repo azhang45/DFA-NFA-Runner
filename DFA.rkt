b494cf9f914008dad1ad1e6d85c3b2549e009201
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2023-03-23 16:00:02 -0400

    NFA works!
    As far as we can tell.
    Tests to come!

--- DFA.rkt
+++ DFA.rkt
@@ -1,24 +1,15 @@
-#lang racket 
+#lang racket
+; (require racket/include)
+(require "NFA.rkt")
 
-
-(define (find-delta-tuple delta curr-state input)
-    (findf 
-        (lambda (d) (and (equal? (car d) curr-state) (equal? (cadr d) input)))
+(define (map-delta delta)
+    (map
+        (lambda (tuple) `(,(first tuple) ,(second tuple) (,(third tuple))))
         delta))
 
-(define (finalstate? s0 F)
-    (cond
-        ((eq? (index-of F s0) #f) #f)
-        (else #t)))   
+(define (DFA2 input Sigma S s0 delta F)
+    (NFA input Sigma S s0 (map-delta delta) F))
 
-(define (DFA input Sigma S s0 delta F)
-    (cond
-        ((= (string-length input) 0)
-            (finalstate? s0 F))
-        (else
-            (define transition    (find-delta-tuple delta s0 (substring input 0 1)))
-
-            (DFA (substring input 1) Sigma S (caddr transition) delta F))))
 
 
 (define test-input "aaaaaaaaaaaaaaaaaabaaab")
@@ -31,6 +22,6 @@
 ))
 (define test-F '(HAPPY))
 
-(DFA test-input test-Sigma test-S test-s0 test-Delta test-F)
+(DFA2 test-input test-Sigma test-S test-s0 test-Delta test-F)
 (displayln "pls work.jpg :tm:")
-(DFA "0100" '("1" "0") '(q0 q1) 'q0 '((q0 "0" q0) (q0 "1" q1) (q1 "0" q1) (q1 "1" q1)) '(q1))
+(DFA2 "0100" '("1" "0") '(q0 q1) 'q0 '((q0 "0" q0) (q0 "1" q1) (q1 "0" q1) (q1 "1" q1)) '(q1))
