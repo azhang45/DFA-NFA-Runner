56085569ed8d822f443205ecd6b331a51fc1192b
azhang45 <anz0063@gmail.com>
2023-04-06 19:41:33 -0400

    eliminated commented out code

--- DFA.rkt
+++ DFA.rkt
@@ -16,18 +16,3 @@
     (execute-NFA (DFA->NFA machine) input))
 
 (provide (all-defined-out))
-
-
-; (define test-input "aaaaaaaaaaaaaaaaaabaaab")
-; (define test-Sigma '("a" "b"))
-; (define test-S '(HAPPY SAD))
-; (define test-s0 'SAD)
-; (define test-Delta '(
-;     (SAD "a" HAPPY) (HAPPY "a" HAPPY)
-;     (SAD "b" SAD) (HAPPY "b" SAD)
-; ))
-; (define test-F '(HAPPY))
-
-; (DFA2 test-input test-Sigma test-S test-s0 test-Delta test-F)
-
-; (DFA2 "0100" '("1" "0") '(q0 q1) 'q0 '((q0 "0" q0) (q0 "1" q1) (q1 "0" q1) (q1 "1" q1)) '(q1))
